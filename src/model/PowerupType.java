285c70b2bae4ec15ab4e57f1c0c8fd4342313899
Piotr Markowski <piotrr.markowski@gmail.com>
2025-05-16 18:46:26 +0200

    update do enuma powerupów

--- src/model/PowerupType.java
+++ src/model/PowerupType.java
@@ -1,8 +1,27 @@
 package model;
 
 public enum PowerupType {
-    LIFE,
-    PEARL,
-    POINTS,
-    SPEED
+    LIFE (0,0),
+    PEARL (0,0),
+    POINTS(0,0),
+    SPEED (0,0);
+
+    private final int   extraPoints;
+    private final int   duration;
+
+    PowerupType(int extraPoints, int duration) {
+        this.extraPoints = extraPoints;
+        this.duration    = duration;
+    }
+
+    
+    public int getExtraPoints() {
+        return extraPoints;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+
 }
